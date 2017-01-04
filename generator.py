d40518296eb06a0061300fdff59643a9d2548f40
Steven <stwalter@redhat.com>
2017-01-04 14:33:59 -0500

    name generation1

--- generator.py
+++ generator.py
@@ -1,2 +1,8 @@
+import random
+
+firstwords = ["Red","Blue","East Street","Death","The"]
+lastwords = ["Falcons","Lightning","Ballers","Families","Crew","Mafia","Mob"]
 def name(input):
-	return "test"
+    first = random.choice(firstwords)
+    last = random.choice(lastwords)
+    return first + " " + last
