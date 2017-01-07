35495faffed5efe07beef3a48fc17af9ada3a55c
Steven <stwalter@redhat.com>
2017-01-07 15:52:42 -0500

    Comprehensive names

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import gang
 import member
 
-gangs = [gang.gang(), gang.gang(), gang.gang()]
 
-print gangs[0].name
-print gangs[1].name
-print gangs[2].name
-print gangs[0].leader.name
-
-
+j = 0
+while j < 50:
+    print gang.gang().name
+    j += 1
 
 i = 0
-while i < 50:
+while i < 0:
     print member.member().name
     i += 1
 #while(0==0):
