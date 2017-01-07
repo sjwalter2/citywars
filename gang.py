5447985f8182c9121fc64b0e184d806e6127a91d
Steven <stwalter@redhat.com>
2017-01-06 19:55:48 -0500

    name generation

--- gang.py
+++ gang.py
@@ -1,17 +1,34 @@
 import random
 import member
 
-class gang(object):
+firstwords = {}
+lastwords = {}
 
+with open("firstwords") as f:
+    i = 0
+    for word in f.readlines():
+        firstwords[i] = word.rstrip()
+        i += 1
+        
+with open("lastwords") as f:
+    i = 0
+    for word in f.readlines():
+        lastwords[i] = word.rstrip()
+        i += 1
+
+def firstname():
+    return firstwords[random.randint(0,len(firstwords)-1)]
+
+def lastname():
+    return lastwords[random.randint(0,len(lastwords)-1)]
+
+
+def name(input):
+    if(input == "gang"):
+        return firstname() + " " + lastname()
+
+class gang(object):
         def __init__(self):
             self.name = name("gang")
             self.leader = member.member()
 
-
-firstwords = ["Red","Blue","East Street","Death","The"]
-lastwords = ["Falcons","Lightning","Ballers","Families","Crew","Mafia","Mob"]
-def name(input):
-    if(input == "gang"):
-        first = random.choice(firstwords)
-        last = random.choice(lastwords)
-        return first + " " + last
